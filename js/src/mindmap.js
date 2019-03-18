d253560b5bc4c6e43a2d6f332003cb828e7eabad
juforg <juforg@sina.com>
2019-03-18 22:47:03 +0800

    Site updated: 2019-03-18 22:47:02

--- js/src/mindmap.js
+++ js/src/mindmap.js
@@ -2,20 +2,21 @@ $(document).ready(function() {
         if ($('.mindmap').length > 0){
           var markdownText = $('.mindmap').text().trim();
           $('.mindmap').text('')
-          var minder = new kityminder.Minder({
+          var minder = window.km = new kityminder.Minder({
               renderTo: '.mindmap'
           });
           minder.useTemplate('right');
 
-          minder.importData('markdown', markdownText);
+          minder.importData('text', markdownText);
           minder.execCommand('Zoom', 90);
 
           minder.disable();
           minder.execCommand('hand');
-
+          minder.execCommand('ExpandToLevel', 2);
           setTimeout(function() {
                   minder.useTemplate('right');
                   minder._viewDragger.move(new kity.Point(-minder._lastClientSize.width / 2.2, 0), 200);
+                  minder.execCommand('ExpandToLevel', 2);
               },
               1000
           )
